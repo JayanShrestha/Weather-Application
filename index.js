cff3239eb3359fcbfa54bc1f2289659511ac189c
JayanShrestha <jayanshr44@gmail.com>
2025-06-16 08:21:08 +1000

    Made changes to overall design and UI

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
-import { api_key } from "./utilities";
+import { api_key } from "./utilities.js";
 
 const app = express();
 const port = 3000;
@@ -12,9 +12,53 @@ const geoAPI_URL = "http://api.openweathermap.org/geo/1.0/direct";
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/",(req,res)=>{
+
+app.get("/", (req,res)=>{
     res.render("index.ejs");
 })
+
+app.post("/curweather", async(req,res)=>{
+try{
+    var location = req.body.location;
+   
+    const geocode = await axios.get(geoAPI_URL,{
+        params:{
+            q:location,
+            appid:api_key,
+        }
+    });
+    const response = JSON.stringify(geocode.data);
+    console.log(response);
+    const result =(geocode.data);
+    console.log(result);
+    console.log(result[0].name);
+
+    var latitude = result[0].lat;
+    var longitude = result[0].lon;
+
+    const weather_details = await axios.get(weatherAPI_URL,{
+        params:{
+            lat:latitude,
+            lon:longitude,
+            appid:api_key,
+            units:"metric",
+            
+            
+        }
+    });
+    console.log(weather_details.data);
+   
+       
+    
+}
+catch(error){
+  console.log("Failed to make connection:", error.message);
+  res.render("index.ejs",{error:error.message})
+}
+res.render("index.ejs",);
+
+})
+
 
 app.listen(port,()=>{
     console.log(`Server is running at ${port}`);
